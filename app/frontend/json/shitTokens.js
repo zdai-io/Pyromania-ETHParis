462186412e1e76f67994b8a101cec1fa067a7e1c
artem <artall64@gmail.com>
2019-03-10 00:30:47 +0200

    add new frontend

--- app/frontend/json/shitTokens.js
+++ app/frontend/json/shitTokens.js
@@ -1,3 +1,87 @@
+var tokens = {
+  mainnet: {
+    1: {
+      "tiker": "FUEL",
+      "name": "FuelToken",
+      "marketCup": 1183110.6,
+      "totalSupply": 2000000000,
+      "address": "0xfcb83a35971680335626287a87B2d10B4234B6Ed"
+    },
+    2: {
+      "tiker": "CAT",
+      "name": "BitClave",
+      "marketCup": 1183110.6,
+      "totalSupply": 2000000000,
+      "address": "0x1234567461d3f8db7496581774bd869c83d51c93"
+    },
+    3: {
+      "tiker": "ASTS",
+      "name": "Token",
+      "marketCup": 42249,
+      "totalSupply": 100000000,
+      "address": "0x2dAEE1AA61D60A252DC80564499A69802853583A"
+    },
+    4: {
+      "tiker": "TNS",
+      "name": "Transcodium",
+      "marketCup": 204993,
+      "totalSupply": 88600000,
+      "address": "0xb0280743b44bf7db4b6be482b2ba7b75e5da096c"
+    },
+    5: {
+      "tiker": "TRST",
+      "name": "Trustcoin",
+      "marketCup": 1200204,
+      "totalSupply": 100000000,
+      "address": "0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b"
+    },
+    6: {
+      "tiker": "BCS",
+      "name": "BCShop.io",
+      "marketCup": 685000,
+      "totalSupply": 10000000,
+      "address": "0x98bde3a768401260e7025faf9947ef1b81295519"
+    }
+  },
+  rinkeby: {
+    1: {
+      "tiker": "CAT",
+      "name": "BitClave",
+      "marketCup": 1183110.6,
+      "totalSupply": 2000000000,
+      "address": "0x1234567461d3f8db7496581774bd869c83d51c93"
+    },
+    2: {
+      "tiker": "ASTS",
+      "name": "Token",
+      "marketCup": 42249,
+      "totalSupply": 100000000,
+      "address": "0x2dAEE1AA61D60A252DC80564499A69802853583A"
+    },
+    3: {
+      "tiker": "TNS",
+      "name": "Transcodium",
+      "marketCup": 204993,
+      "totalSupply": 88600000,
+      "address": "0xb0280743b44bf7db4b6be482b2ba7b75e5da096c"
+    },
+    4: {
+      "tiker": "TRST",
+      "name": "Trustcoin",
+      "marketCup": 1200204,
+      "totalSupply": 100000000,
+      "address": "0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b"
+    },
+    5: {
+      "tiker": "BCS",
+      "name": "BCShop.io",
+      "marketCup": 685000,
+      "totalSupply": 10000000,
+      "address": "0x98bde3a768401260e7025faf9947ef1b81295519"
+    }
+  }
+};
+
 var shitTokens = [
   {
     "tiker": "FUEL",
@@ -41,4 +125,6 @@ var shitTokens = [
     "totalSupply": 10000000,
     "address": "0x98bde3a768401260e7025faf9947ef1b81295519"
   }
-]
+];
+
+
