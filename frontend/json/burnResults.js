d701d8770e141cd5a5c7bd80ae99c41a63e31972
Arsenii Pechenkin <arspechen@gmail.com>
2019-03-10 00:46:08 +0300

    add table

---
+++ frontend/json/burnResults.js
@@ -0,0 +1,42 @@
+var burnResults = [
+  {
+    "tokenAddress": "0x1234567461d3f8db7496581774bd869c83d51c93",
+    "velocity": 1,
+    "burnCount": 12,
+    "totalCount": 10000000,
+    "minBurnPrice": 2,
+    "blockHeight": 34567
+  },
+  {
+    "tokenAddress": "0x2dAEE1AA61D60A252DC80564499A69802853583A",
+    "velocity": 1,
+    "burnCount": 12,
+    "totalCount": 10000000,
+    "minBurnPrice": 2,
+    "blockHeight": 34567
+  },
+  {
+    "tokenAddress": "0xb0280743b44bf7db4b6be482b2ba7b75e5da096c",
+    "velocity": 1,
+    "burnCount": 12,
+    "totalCount": 10000000,
+    "minBurnPrice": 2,
+    "blockHeight": 34567
+  },
+  {
+    "tokenAddress": "0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b",
+    "velocity": 1,
+    "burnCount": 12,
+    "totalCount": 10000000,
+    "minBurnPrice": 2,
+    "blockHeight": 34567
+  },
+  {
+    "tokenAddress": "0x98bde3a768401260e7025faf9947ef1b81295519",
+    "velocity": 1,
+    "burnCount": 12,
+    "totalCount": 10000000,
+    "minBurnPrice": 2,
+    "blockHeight": 34567
+  }
+]
